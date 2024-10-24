0204feaa727759244d8c2218ff8722fbd64b5ce9
Mujtaba Ibrahim <127867695+mujtaba98iq@users.noreply.github.com>
2024-10-24 12:08:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{ 
+    options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme,
+    securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter The Bearer Authorization : 'Bearer Generated-JWT-Token'",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {{
+        new OpenApiSecurityScheme
+        {
+             Reference=new OpenApiReference
+             {
+                 Type=ReferenceType.SecurityScheme,
+                 Id=JwtBearerDefaults.AuthenticationScheme
+             }
+        },new string[]{}
+        }
+    });
+
+   } ); 
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
